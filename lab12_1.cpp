4e7a4881fa7f767e36d1d2f0554bea1019f0652d
= <=>
2023-01-06 16:03:34 +0700

    lab12_1_completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,37 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <ctime>
+#include <string>
+using namespace std ;
+
+int main () {
+    int random ;
+    string grade;
+    srand(time(0));
+cout << "Press Enter 3 times to reveal your future.";
+cin.get() ;
+cin.get() ;
+cin.get() ;
+random =(rand()%9)+1 ;
+    if (random == 1){
+        grade = "A" ;
+    }else if(random == 2) {
+        grade = "B+" ;
+    }else if(random == 3 ) {
+        grade = "B" ;
+    }else if(random == 4 ) {
+        grade = "C+" ; 
+    }else if (random == 5) {
+        grade = "C" ;
+    }else if (random == 6) {
+        grade = "D+" ;
+    }else if (random == 7) {
+        grade = "D" ;
+    }else if (random == 8) {
+        grade = "F" ;
+    }else if (random == 9) {
+        grade = "W" ; 
+    }
+    cout << "You will get "<< grade <<" in this 261102.";
+    return 0 ;
+}
+
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+using namespace std ;
+
+int main ()
+{
+    srand (time(0)) ;
+cout<< rand()<<endl ;
+
+}
